e595ec4a1b4c4afab1dca50008da10534f0e54b3
nikitaSisikin <nikita.sisikin@outlook.com>
2023-12-19 04:00:51 +0000

    added textarea to paste code

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+placeholder_code = "print('Hello, World!')"
+
 @app.route("/", methods=["GET"])
 def code():
     context = {
         "message": "Paste your Python code here 🐍",
+        "code": placeholder_code,
     }
     return render_template("code_input.html", **context)
